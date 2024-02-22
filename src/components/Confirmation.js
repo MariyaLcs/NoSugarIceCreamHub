d3ad658ce06c190adcd15f6abbe8113e00f4f982
Mariya Podosinova <mariya.podosinova@gmail.com>
2024-02-22 14:55:01 +0000

    update confirmation page for new message layout

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -4,16 +4,16 @@ export default function Confirmation() {
   const { state } = useLocation();
   return (
     <div className="container">
-      {state && (
-        <p>
-          Dear <strong>{state.name}</strong>,
-        </p>
-      )}
       <h1>Thank you for subscribing 🍦</h1>
       {state && (
-        <p>
-          We will contact you soon at <strong>{state.phone}</strong>
-        </p>
+        <>
+          <p>
+            Dear <strong>{state.name}</strong>,
+          </p>
+          <p>
+            We will contact you soon at <strong>{state.phone}</strong>
+          </p>
+        </>
       )}
     </div>
   );
